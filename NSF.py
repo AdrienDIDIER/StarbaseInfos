bf131073bc5b207c1fce05e4cbb937b8cd7a639c
Adrien <adrien.didier@outlook.fr>
2023-07-15 10:59:41 +0200

    Init

---
+++ NSF.py
@@ -0,0 +1,79 @@
+import pytesseract
+import webcolors
+import re
+
+from utils.color_detector import BackgroundColorDetector
+from utils.utils import get_database, get_api_twitter, set_last_tweet, get_last_tweet
+from vidgear.gears import CamGear
+
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+def closest_colour(requested_colour):
+    min_colours = {}
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+
+def get_colour_name(requested_colour):
+    try:
+        closest_name, actual_name = webcolors.rgb_to_name(requested_colour)
+    except Exception as e:
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
+
+
+def img_to_text(crop_frame):
+
+    BackgroundColor = BackgroundColorDetector(crop_frame)
+    _, closest_name = get_colour_name(BackgroundColor.detect())
+    
+    if closest_name == 'firebrick':
+        text = str(((pytesseract.image_to_string(crop_frame))))
+        textEN = text.replace("-\n", "")
+        return textEN
+    else:
+        return None
+
+
+def getScreenNSF(url):
+    stream = CamGear(source=url, stream_mode=True, logging=False).start() # YouTube Video URL as input
+    frame = stream.read()
+    crop_frame = frame[995:1080, 245:1820]
+    ret = img_to_text(crop_frame)
+    if ret==None or '@nasaspaceflight' in ret.lower():
+        return None
+    else:
+        ret = ret.replace("$", "S")
+        return "Infos @NASASpaceflight : \n" + ret
+
+def check_NSF(api, db_client, text):
+    if not get_last_tweet(db_client, re.sub(r'[^\w\s]', '', text).lower(), "MONGO_DB_URL_TABLE_PT"):
+        print('Tweet NSF')
+        try:
+            api.create_tweet(text="🇺🇸 " + text)
+        except Exception as e:
+            print(e)
+        set_last_tweet(db_client, re.sub(r'[^\w\s]', '', text).lower(), "MONGO_DB_URL_TABLE_PT")
+    else:
+        print('No Tweet NSF')
+
+
+
+def run_NSF():
+    db = get_database()
+    api = get_api_twitter()
+    
+    textNSF = getScreenNSF("https://www.youtube.com/watch?v=mhJRzQsLZGg")
+    if textNSF is not None:
+        check_NSF(api, db, textNSF)
+    else:
+        print('No Tweet NSF')
+
+run_NSF()
---
+++ utils/utils.py
@@ -0,0 +1,38 @@
+import tweepy
+import os
+import dotenv
+
+from pymongo import MongoClient
+
+dotenv.load_dotenv()
+
+
+def get_database():
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = os.getenv('MONGO_DB_URL')
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING)
+    return client['bocachicaroadclosure']
+
+
+def get_api_twitter():
+    # Authenticate to Twitter
+    client = tweepy.Client(
+        consumer_key=os.getenv("TWITTER_API_KEY"), consumer_secret=os.getenv("TWITTER_API_SECRET_KEY"),
+        access_token=os.getenv("TWITTER_ACCESS_TOKEN"), access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+    )
+    return client
+
+
+def set_last_tweet(client, id, table):
+    client[os.getenv(table)].replace_one(
+        {"last_id":id}, {"last_id":id}, upsert=True
+    )
+    return 
+
+
+def get_last_tweet(client, id, table):
+    res = client[os.getenv(table)].find_one({"last_id": id})
+    if res is not None:
+        return True
+    return False
