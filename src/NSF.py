d77b9b8f789391fb9e6247b7e36a00697fd5d4ee
Adrien <adrien.didier@outlook.fr>
2025-08-16 22:08:06 +0200

    comment

--- src/NSF.py
+++ src/NSF.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 
 
